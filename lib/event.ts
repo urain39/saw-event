2d0f566bcd0fb9b3565723019f2bd3c1cbeed191
urain39 <urain39@qq.com>
2021-02-07 13:38:05 +0800

    init repo

---
+++ lib/event.ts
@@ -0,0 +1,79 @@
+type IMap<V> = {
+    [index: number]: V;
+    [key: string]: V;
+};
+
+type SAWEventKey = number | string;
+
+type SAWEventValue = number | string | boolean | null;
+
+/**
+ * @param data 事件传递的数据
+ */
+export type SAWEventCallback = (data?: any) => any;
+
+export class SAWEvent {
+    private static _eventValueMap: IMap<SAWEventValue> = {};
+    private static _eventCallbackMap: IMap<[SAWEventValue, SAWEventCallback][]> = {};
+
+    /**
+     * 添加一个事件监听器。
+     * @param key 监听的键
+     * @param value 想要的值
+     * @param callback 当键对应的值达到想要的值时执行的回调
+     */
+    public static on(key: SAWEventKey, value: SAWEventValue, callback: SAWEventCallback): void {
+        const eventCallbackMap = this._eventCallbackMap;
+
+        let callbacks = eventCallbackMap[key];
+        if (!callbacks) {
+            eventCallbackMap[key] = callbacks = [];
+        }
+
+        callbacks.push([value, callback]);
+    }
+
+    /**
+     * 移除一个事件监听器。
+     * @param key 监听的键
+     * @param value 想要的值
+     * @param callback 当键对应的值达到想要的值时执行的回调
+     */
+    public static off(key: SAWEventKey, value: SAWEventValue, callback: SAWEventCallback): void {
+        const eventCallbackMap = this._eventCallbackMap;
+        const callbacks = eventCallbackMap[key];
+
+        if (callbacks) {
+            for (let i = 0, l = callbacks.length; i < l; i++) {
+                const [vaule_, callback_] = callbacks[i];
+                if (vaule_ === value && callback_ === callback) {
+                    callbacks.splice(i, 1);
+                    break;
+                }
+            }
+        }
+    }
+
+    /**
+     * 设置一个键值，并执行相关的回调。
+     * @param key 与监听器对应的键
+     * @param value 键对应的值
+     * @param data 发送给回调的数据
+     */
+    public static set(key: SAWEventKey, value: SAWEventValue, data?: any) {
+        const eventValueMap = this._eventValueMap;
+
+        eventValueMap[key] = value;
+
+        const eventCallbackMap = this._eventCallbackMap;
+
+        let callbacks = eventCallbackMap[key];
+        if (callbacks) {
+            for (const [wantedVaule, callback] of callbacks) {
+                if (wantedVaule === value) {
+                    callback(data);
+                }
+            }
+        }
+    }
+}
---
+++ test.ts
@@ -0,0 +1,68 @@
+import { SAWEvent, SAWEventCallback } from './index';
+
+let callback1: SAWEventCallback;
+SAWEvent.on('wakeUp', true, callback1 = function (data) {
+    console.log(`Wake Up x1`);
+
+    if (data !== void 0) {
+        console.log(`Received data ${data}`);
+    }
+});
+
+console.log(`Event: Wake Up!`);
+SAWEvent.set('wakeUp', true);
+
+// @ts-ignore
+let callback2: SAWEventCallback;
+SAWEvent.on('wakeUp', true, callback2 = function (data) {
+    console.log(`Wake Up x2`);
+
+    if (data !== void 0) {
+        console.log(`Received data ${data}`);
+    }
+});
+
+console.log(`Event: Wake Up!(send cookie)`);
+SAWEvent.set('wakeUp', true, `Cookie`);
+
+console.log(`Event: Remove callback1(wakeup1)`);
+SAWEvent.off('wakeUp', true, callback1);
+
+// @ts-ignore
+let callback3: SAWEventCallback;
+SAWEvent.on('wakeUp', true, callback3 = function (data) {
+    console.log(`Wake Up x3`);
+
+    if (data !== void 0) {
+        console.log(`Received data ${data}`);
+    }
+});
+
+console.log(`Event: Wake Up!`);
+SAWEvent.set('wakeUp', true);
+
+SAWEvent.on('wakeUp', false, function (data) {
+    console.log(`Sleep x1`);
+
+    if (data !== void 0) {
+        console.log(`Received data ${data}`);
+    }
+});
+
+let callback4: SAWEventCallback;
+SAWEvent.on('wakeUp', false, callback4 = function (data) {
+    console.log(`Sleep x2`);
+
+    if (data !== void 0) {
+        console.log(`Received data ${data}`);
+    }
+});
+
+console.log(`Event: Sleep!`);
+SAWEvent.set(`wakeUp`, false);
+
+console.log(`Event: Remove callback4(sleep2)`);
+SAWEvent.off('wakeUp', false, callback4);
+
+console.log(`Event: Sleep!(send cookie2)`);
+SAWEvent.set('wakeUp', false, `Cookie2`);
